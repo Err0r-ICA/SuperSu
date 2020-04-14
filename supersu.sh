7143fd8c1daa0d470699b337f6981497ccfc2ae1
*ERR0R* <59941931+Err0r-ICA@users.noreply.github.com>
2020-04-14 05:51:54 +0200

    Add files via upload

---
+++ supersu.sh
@@ -0,0 +1,258 @@
+#!/bin/bash
+# Kali Linux Distro Installer
+# ITALIA CYBER ARMY PRODUCTION
+# 
+white='\e[1;97m'
+cyan='\e[1;96m'
+purple='\e[1;95m'
+blue='\e[1;94m'
+yellow='\e[1;93m'
+green='\e[1;92m'
+red='\e[1;91m'
+clear
+echo ""
+
+echo "Ciao!" |lolcat
+sleep 1
+echo ""
+echo "Welcome Into The Unix Dasboards" |lolcat
+sleep 2
+echo ""
+
+echo -e  "
+
+\033[1;92m  ║\033[0;32m▒▒▒▒▒▒▒▒▒▒\033[1;92m║
+\033[1;92m  ║\033[0;32m▒▒▒▒▒▒▒▒▒▒\033[1;92m║
+\033[1;92m  ║\033[0;32m▒▒▒▒▒▒▒▒▒▒\033[1;92m║
+\033[1;92m  ║\033[0;32m▒▒▒▒▒▒▒▒▒▒\033[1;92m║
+\033[1;92m  ║\033[0;32m▒▒▒▒▒▒▒▒▒▒\033[1;92m║
+\033[1;92m  ║\033[0;32m▒▒▒▒▒▒▒▒▒▒\033[1;92m║
+\033[1;92m ╔════════════╗
+\033[1;96m termux_hacking
+\033[1;92m ╚════════════╝
+\033[1;92m  ║\033[1;97m██████████\033[1;92m╚╗
+\033[1;92m  ║\033[1;97m██\033[1;92m╔══╗\033[1;97m█\033[1;92m╔═╗\033[1;97m█\033[1;92m║
+\033[1;92m  ║\033[1;97m██\033[1;92m║\033[1;95m╬\033[1;92m╔╝\033[1;97m█\033[1;92m╚╗║\033[1;97m█\033[1;92m║
+\033[1;92m  ║\033[1;97m██\033[1;92m╚═╝\033[1;97m█\033[1;90m║\033[1;97m█\033[1;92m╚╝\033[1;97m█\033[1;92m║
+\033[1;92m  ╚╗\033[1;97m█████████\033[1;92m═╝
+\033[1;92m   ╚╗║\033[1;90m╠╩╩╩╩╩╝
+\033[1;92m     ║║┈┈┈\033[1;93m███\033[1;97m▐█████████\033[1;91m▒\033[1;30m.｡oO
+\033[1;92m     ║\033[1;97m██\033[1;90m╠╦╦╦╗
+\033[1;92m     ╚╗\033[1;97m██████";
+
+echo ""
+sleep 1
+echo ""
+echo -e $green"|\033[1;97m+\033[1;95m###########\033[1;96m(\033[1;97mDISTRO\033[1;96m)\033[1;95m#########\033[1;97m%\033[1;92m|"
+echo -e $green"|\033[1;97m+\033[1;92mTOOLS  : \033[1;96mUNIX DASHBOARD     \033[1;97m+\033[1;92m|"
+echo -e $green"|\033[1;97m+\033[1;97mAUTHOR : \033[1;95m*\033[1;97mERR0R\033[1;95m*            \033[1;97m+\033[1;92m|"
+echo -e $green"|\033[1;97m+\033[1;91mINSTAGRAM : \033[1;93m@termux_hacking \033[1;97m+\033[1;92m|"
+echo -e $green"|\033[1;97m+\033[1;96mTELEGRAM  : \033[1;94m/termuxxhacking \033[1;97m+\033[1;92m|"
+echo -e $green"|\033[1;97m+\033[1;92mRun PC Terminals, NO Root   \033[1;97m+\033[1;92m|"
+echo -e $green"|\033[1;97m~\033[1;97mSpecial Thanks to I.C.A.    \033[1;97m+\033[1;92m|"
+echo -e $green"|\033[1;97m°\033[1;91mAnd All My Followers        \033[1;97m+\033[1;92m|"
+echo -e $green"|\033[1;97m%\033[1;95m###########\033[1;96m[\033[1;97mADMIN\033[1;96m]\033[1;95m##########\033[1;97m%\033[1;92m|"
+echo ""
+sleep 1
+echo -e $white">>\033[1;95m==========\033[1;91m{\033[1;93mMENU\033[1;91m}\033[1;95m==========\033[1;97m<<"
+echo -e "\033[1;95m[\033[1;97m01\033[1;95m]\033[1;92m.Ubuntu"
+sleep 0.2
+echo -e "\033[1;90m∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m02\033[1;95m]\033[1;96m.Debian"
+sleep 0.2
+echo -e "\033[1;90m≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m03\033[1;95m]\033[1;92m.Kali Linux"
+sleep 0.2
+echo -e "\033[1;90m≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m04\033[1;95m]\033[1;96m.Kali Nethunter"
+sleep 0.2
+echo -e "\033[1;90m∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m05\033[1;95m]\033[1;92m.Parrot Security OS"
+sleep 0.2
+echo -e "\033[1;90m≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m06\033[1;95m]\033[1;96m.BackBox"
+sleep 0.2
+echo -e "\033[1;90m≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m07\033[1;95m]\033[1;92m.Fedora"
+sleep 0.2
+echo -e "\033[1;90m∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m08\033[1;95m]\033[1;96m.CentOS"
+sleep 0.2
+echo -e "\033[1;90m≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m09\033[1;95m]\033[1;92m.openSUSE Leap"
+sleep 0.2
+echo -e "\033[1;90m≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m10\033[1;95m]\033[1;96m.openSUSE Tumbleweed"
+sleep 0.2
+echo -e "\033[1;90m∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m11\033[1;95m]\033[1;92m.Arch Linux"
+sleep 0.2
+echo -e "\033[1;90m≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m12\033[1;95m]\033[1;96m.Black Arch"
+sleep 0.2
+echo -e "\033[1;90m≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m13\033[1;95m]\033[1;92m.ANDRAX"
+sleep 0.2
+echo -e "\033[1;90m∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m14\033[1;95m]\033[1;96m.Info"
+sleep 0.2
+echo -e "\033[1;90m≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\033[1;97m»»"
+echo -e "\033[1;95m[\033[1;97m00\033[1;95m]\033[1;91m.Exit"
+sleep 0.2
+echo -e $purple"≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠»»\033[1;92m"
+echo ""
+echo ""
+sleep 1
+echo ""
+read -p"Choose Number -->>>"  tpw;
+
+if [ $tpw = 1 ] || [ $tpw = 1 ]
+then
+echo "Installing...!" | lolcat
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
+./start-ubuntu.sh
+fi
+
+if [ $tpw = 2 ] || [ $tpw = 2 ]
+then
+echo "Installing...!"|lolcat
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Debian/debian.sh && bash debian.sh
+./start-debin.sh
+fi
+
+if [ $tpw = 3 ] || [ $tpw = 3 ]
+then
+echo "Installing...!"|lolcat
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+./start-kali.sh
+fi
+
+if [ $tpw = 4 ] || [ $tpw = 4 ]
+then
+echo "Installing...!"|lolcat
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Nethunter/nethunter.sh && bash nethunter.sh
+./start-nethunter.sh
+fi
+
+if [ $tpw = 5 ] || [ $tpw = 5 ]
+then
+echo "Installing...!"|lolcat
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Parrot/parrot.sh && bash parrot.sh
+./start-parrot.sh
+fi
+
+if [ $tpw = 6 ] || [ $tpw = 6 ]
+then
+echo "Installing...!"|lolcat
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/BackBox/backbox.sh && bash backbox.sh
+./start-backbox.sh
+fi
+
+if [ $tpw = 7 ] || [ $tpw = 7 ]
+then
+echo " Installing...!"|lolcat
+pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Fedora/fedora.sh && bash fedora.sh
+./start-fedora.sh
+fi
+
+if [ $tpw => 8 ] || [ $tpw = 8 ]
+then
+echo " Installing...!"|lolcat
+pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/CentOS/centos.sh && bash centos.sh
+./start-centos.sh
+fi
+
+if [ $tpw = 9 ] || [ $tpw = 9 ]
+then
+echo " Installing...!"|lolcat
+pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/openSUSE/Leap/opensuse-leap.sh && bash opensuse-leap.sh
+./start-leap.sh
+fi
+
+if [ $tpw = 10 ] || [ $tpw = 10 ]
+then
+echo " Installing...!"|lolcat
+pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/openSUSE/Tumbleweed/opensuse-tumbleweed.sh && bash opensuse-tumbleweed.sh
+./start-tumbleweed.sh
+fi
+
+if [ $tpw = 11 ] || [ $tpw = 11 ]
+then
+echo " Installing...!"|lolcat
+pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Arch/armhf/arch.sh && bash arch.sh
+./start-arch.sh
+fi
+
+if [ $tpw = 12 ] || [ $tpw = 12 ]
+then
+echo " Installing...!"|lolcat
+pacman-key --init && pacman-key --populate archlinuxarm && pacman -Sy --noconfirm curl && curl -O https://blackarch.org/strap.sh && chmod +x strap.sh && ./strap.sh
+./start-arch.sh
+fi
+
+if [ $tpw = 13 ] || [ $tpw = 13 ]
+then
+echo " Installing...WARNING: Root Rrequired!"|lolcat
+termux-open-url "https://andrax.thecrackertechnology.com/download"
+fi
+
+if [ $tpw = 14 ] || [ $tpw = 14 ]
+then
+echo " Credits&Info" |lolcat
+echo ""
+sleep 0.5
+echo "Hello Hacker, Nice to See You."| lolcat
+sleep 0.2
+echo ""
+echo "Follow on Instagram"| lolcat
+sleep 0.2
+echo ""
+echo "And Join our Group on Telegram"| lolcat
+sleep 0.2
+echo ""
+echo "This Script Can Install"| lolcat
+sleep 0.2
+echo ""
+echo "Most Important Unix Distro"| lolcat
+sleep 0.2
+echo ""
+echo "I Highly Recommend Kali Distro !"| lolcat
+sleep 0.2
+echo ""
+echo "Choose Your Favourite And Have Fun !"| lolcat
+sleep 0.2
+echo ""
+echo "Regards From:\n" |lolcat
+sleep 0.2
+echo ""
+echo "Err0r  -  Italia Cyber Army"| lolcat
+sleep 0.2
+echo ""
+echo "@Instagram: termux_hacking"| lolcat
+sleep 1
+termux-open-url "https://www.instagram.com/termux_hacking/?hl=it"
+fi
+
+
+
+if [ $tpw = 00 ] || [ $tpw = 00 ]
+then
+clear
+echo "Nice Scripts Huh?"|lolcat
+echo ""
+sleep 1
+echo "Thanks to: **ITALIA CYBER ARMY**"| lolcat
+echo ""
+toilet -f future "-+- Err0r -+-"| lolcat
+echo ""
+sleep 1
+toilet -f future "Bye Bye !"| lolcat
+echo ""
+echo "IG: @termux_hacking"| lolcat
+echo ""
+sleep 1
+echo "Do NOT Re-Code !!!"| lolcat
+echo ""
+sleep 1
+echo "See You Next Time !"| lolcat
+fi
